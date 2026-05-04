5803628942530dfd8b8459281acd54493f1460b9
fabriziomercurio <mercurio.fabrizio@gmail.com>
2026-05-04 14:35:57 +0200

    feat:upload image on product form

--- src/components/products/AddProduct.tsx
+++ src/components/products/AddProduct.tsx
@@ -4,15 +4,18 @@ const baseUrl = import.meta.env.VITE_BASE_URL;
 const AddProduct = () => { 
  
     type productForm = {
-    title: string; 
+    title: string;
+    image:Blob|null
   }
 
-  const [formData,setFormData] = useState<productForm>({"title":""}) 
+  const [formData,setFormData] = useState<productForm>({"title":"","image":null}) 
 
   const handleInputChange = (e:React.ChangeEvent<HTMLInputElement>)  => 
   {
-     const {name, value} = e.target; 
-     setFormData({...formData,  [name]: value,}); 
+     const {name, value, type, files} = e.target; 
+     setFormData({...formData,    
+      [name]: type === 'file' && files ? files[0] : value // if files exists use files[0] otherwise use value      
+    });  
   } 
 
    const submitForm = async (e:React.FormEvent<HTMLFormElement>) => { 
@@ -20,14 +23,20 @@ const AddProduct = () => {
     try { 
     const res = new FormData(); 
     res.append("title", formData.title); 
+    res.append("image", formData.image ?? '')
 
-    const data = await fetch(`${baseUrl}/products`, {
+    const data = await fetch(`${baseUrl}/api/products`, {
       method:'POST', 
       body:res
-    }); 
-     
-    const test = await data.json();  
-    console.log(test);
+    });        
+
+    if (!data.ok) {
+      const text = await data.text();
+      throw new Error(text); 
+    }
+
+    const result = await data.json();  
+    console.log(result);
          
     } catch (error) {
         console.log("Err:", error); 
@@ -37,7 +46,8 @@ const AddProduct = () => {
    return(<>
    <form onSubmit={submitForm}>
     <label htmlFor="title">Title:</label><br />
-    <input type="text" id="title" name="title" onChange={handleInputChange} /><br />
+    <input type="text" id="title" name="title" onChange={handleInputChange} /><br /> 
+    <input type="file" name="image" onChange={handleInputChange}/>
     <button type="submit">click</button>
     </form>
    </>)
